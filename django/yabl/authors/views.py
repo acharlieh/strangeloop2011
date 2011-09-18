651bc2bd26f0006bcd8b01dbb294842980c7ceb0
Charlie Huggard-Lee <charlie@huggard-lee.net>
2011-09-18 17:27:39 -0500

    Use a template by hand.

--- django/yabl/authors/views.py
+++ django/yabl/authors/views.py
@@ -1,10 +1,10 @@
 from django.http import HttpResponse
 from yabl.authors.models import Author
+from django import template
 # Create your views here.
 
 def author_list(request):
-    response = '<ul>'
-    for author in Author.objects.all():
-        response += '<li>%s</li>' % author
-    response += '</ul>'
+    tmpl = template.loader.get_template('author_list.html')
+    context = template.Context({'authors': Author.objects.all()})
+    response = tmpl.render(context)
     return HttpResponse(response)
