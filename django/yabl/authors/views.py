9a84f26eeacccda417eede3d6794c1c167e1783a
Charlie Huggard-Lee <charlie@huggard-lee.net>
2011-09-18 17:21:33 -0500

    create a view

--- django/yabl/authors/views.py
+++ django/yabl/authors/views.py
@@ -1,1 +1,10 @@
+from django.http import HttpResponse
+from yabl.authors.models import Author
 # Create your views here.
+
+def author_list(request):
+    response = '<ul>'
+    for author in Author.objects.all():
+        response += '<li>%s</li>' % author
+    response += '</ul>'
+    return HttpResponse(response)
