e92482231598b977f07c2681f20b5d9c83b6560d
Charlie Huggard-Lee <charlie@huggard-lee.net>
2011-09-18 16:19:18 -0500

    add a unicode method for the authors model

--- django/yabl/authors/models.py
+++ django/yabl/authors/models.py
@@ -6,3 +6,6 @@ class Author(models.Model):
     last_name = models.CharField(max_length=200)
     bio = models.TextField(blank=True)
 
+    def __unicode__(self):
+        return '%s %s' % (self.first_name, self.last_name)
+
